98df61e150d0c2406ec77fca30c7d76039890fb1
marineasiryan <marinasiryan14@gmail.com>
2022-10-07 18:32:07 +0400

    array with differnt types values

--- concat_str.cpp
+++ concat_str.cpp
@@ -2,8 +2,7 @@
 #include <cstring>
 
 void concat_str(char* str1, char *str2){
-    int i;
-    for( i = 0; str1[i] != '\0'; ++i);
+        int i=strlen(str1);
     
         for (int j = 0; str2[j] != '\0'; ++j, ++i){
             str1[i] = str2 [j];
